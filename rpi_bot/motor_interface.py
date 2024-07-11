05452ee473517b9e73b6aa496763b55966cfec57
jajkor <play4rj@aol.com>
2024-07-11 15:45:13 -0400

    Adjusted PWM frequency

--- rpi_bot/motor_interface.py
+++ rpi_bot/motor_interface.py
@@ -20,8 +20,8 @@ class RPi_Motors(object):
 		GPIO.setup(self.ENA,GPIO.OUT)
 		GPIO.setup(self.ENB,GPIO.OUT)
 		self.forward(1)
-		self.PWMA = GPIO.PWM(self.ENA,500)
-		self.PWMB = GPIO.PWM(self.ENB,500)
+		self.PWMA = GPIO.PWM(self.ENA,50)
+		self.PWMB = GPIO.PWM(self.ENB,50)
 		self.PWMA.start(0)
 		self.PWMB.start(0)
 
