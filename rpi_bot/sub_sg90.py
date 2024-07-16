55ca22e5928a854622083a8288ec93010d1ee15b
jajkor <play4rj@aol.com>
2024-07-15 21:03:40 -0400

    Added angle range checking and hardware functionality

--- rpi_bot/sub_sg90.py
+++ rpi_bot/sub_sg90.py
@@ -14,21 +14,24 @@ class SG90_Subscriber(Node):
             self.get_parameter('pwm_pin').get_parameter_value().integer_value
         )
 
-        angle = 0
+        self.angle = 0
 
         self.subscription = self.create_subscription(Joy, 'joy', self.cmd_to_angle_callback, 10)
         self.subscription  # prevent unused variable warning
         self.get_logger().info('SG90 Subscriber Initialized')
 
     def cmd_to_angle_callback(self, msg):
-        if (self.angle < 90) and (self.angle > -90):
-            if (msg.buttons[4] == "1") and (msg.buttons[5] == "0"):
-                self.angle += 0.1
-            elif (msg.buttons[4] == "0") and (msg.buttons[5] == "1"):
-                self.angle -= 0.1
+        if (self.angle > -90):
+            if (msg.buttons[4] == 1) and (msg.buttons[5] == 0):
+                self.angle -= 1
+        if (self.angle < 90):
+            if (msg.buttons[4] == 0) and (msg.buttons[5] == 1):
+                self.angle += 1
 
-        self.get_logger().info('Angle: "%s"' % str(self.angle))
-        self.get_logger().info('Buttons: "%s"' % str(msg.buttons[4]))
+        self.sg90.set_angle(self.angle)
+
+        self.get_logger().info(f'Angle: {self.angle}')
+        self.get_logger().info(f'Left: {str(msg.buttons[4])}, Right: {str(msg.buttons[5])}')
 
 
 def main(args=None):
