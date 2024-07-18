b5f3df6f7ec08db388f5a28b0b71473caeeb3a4e
jajkor <play4rj@aol.com>
2024-07-18 09:43:55 -0400

    Improved logging

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
             'pub_hcs04_node = rpi_bot.pub_hcs04:main',
             'sub_hcs04_node = rpi_bot.sub_hcs04:main',
             'sub_sg90_node = rpi_bot.sub_sg90:main',
-            'sub_lo_node = rpi_bot.sub_log:main'
+            'sub_log_node = rpi_bot.sub_log:main'
         ],
     },
 )
