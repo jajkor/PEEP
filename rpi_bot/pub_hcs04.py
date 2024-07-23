5608a67b934633411a5b0890331082ec921bc1d9
jajkor <play4rj@aol.com>
2024-07-22 21:14:32 -0400

    Sensor

--- rpi_bot/pub_hcs04.py
+++ rpi_bot/pub_hcs04.py
@@ -13,7 +13,6 @@ class HCS04_Publisher(Node):
             reliability=QoSReliabilityPolicy.BEST_EFFORT,
             history=QoSHistoryPolicy.KEEP_LAST,
             depth=5,
-            durability=QoSDurabilityPolicy.VOLATILE,
         )
 
         self.declare_parameters(
