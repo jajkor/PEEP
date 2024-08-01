61449132d5f9114a6aaca39d9824da8194699bb6
jajkor <play4rj@aol.com>
2024-08-01 13:48:29 -0400

    Working on auto nav

--- launch/rpi_launch.py
+++ launch/rpi_launch.py
@@ -66,19 +66,4 @@ def generate_launch_description():
                 'axes_btn': 4
             }]
         ),
-        '''
-        launch_ros.actions.Node(
-            package='usb_cam',
-            executable='usb_cam_node_exe',
-            name='usb_cam',
-            output='screen',
-            parameters=[{
-                'video_device': '/dev/video0',
-                'image_width': 640,
-                'image_height': 480,
-                'pixel_format': 'yuyv',
-                'camera_frame_id': 'camera_frame',
-            }],
-        ),
-        '''
     ])
