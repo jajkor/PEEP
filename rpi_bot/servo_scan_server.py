48aababbeb2612a50cc06c2c4929d593104711e0
jajkor <play4rj@aol.com>
2024-08-04 11:05:03 -0400

    Added Callback groups to Scan Service

--- rpi_bot/servo_scan_server.py
+++ rpi_bot/servo_scan_server.py
@@ -4,6 +4,8 @@ from rclpy.node import Node
 from sensor_msgs.msg import Range
 from rpi_bot.rpi_interface import RPi_SG90
 from rpi_bot_interfaces.srv import Scan
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup, ReentrantCallbackGroup
 
 class Servo_Scan(Node):
 
@@ -25,15 +27,16 @@ class Servo_Scan(Node):
         self.is_busy = False
         self.distance = None
 
-        self.srv = self.create_service(Scan, 'servo_scan', self.scan_callback)
-        self.range_listener = self.create_subscription(Range, 'range', self.scan_callback, 10)
+        self.callback_group = ReentrantCallbackGroup()
+        self.srv = self.create_service(Scan, 'servo_scan', self.scan_callback, callback_group=self.callback_group)
+        self.range_listener = self.create_subscription(Range, 'range', self.range_listener_callback, 10,  callback_group=self.callback_group))
 
     def range_listener_callback(self, range_msg):
         self.distance = range_msg.range * 17150
         self.distance = round(self.distance, 2)
         self.get_logger().info(f'Updated distance: {self.distance}')
 
-    def scan_callback(self, request, response, range_msg):
+    def scan_callback(self, request, response):
         if self.is_busy:
             self.get_logger().info('Service is busy. Cannot handle request: %s' % request.request_data)
             response.response_data = 'Service is currently busy. Please try again later.'
@@ -45,9 +48,8 @@ class Servo_Scan(Node):
         for i in range(int(request.start_angle), int(request.stop_angle), 10):
             self.sg90.set_angle(i)
             time.sleep(0.5)
-            if self.distance is not None:
-                response.list_angle.append(float(self.sg90.get_angle()))
-                response.list_distance.append(float(round(range_msg.range * 17150, 2)))
+            response.list_angle.append(float(self.sg90.get_angle()))
+            response.list_distance.append(float(self.distance))
 
         self.is_busy = False
         self.sg90.set_angle(90)
@@ -60,7 +62,14 @@ def main(args=None):
     rclpy.init(args=args)
 
     servo_scan = Servo_Scan()
-    rclpy.spin(servo_scan)
+    executor = MultiThreadedExecutor()
+    executor.add_node(servo_scan)
+
+    try:
+        servo_scan.get_logger().info('Beginning client, shut down with CTRL-C')
+        executor.spin()
+    except KeyboardInterrupt:
+        servo_scan.get_logger().info('Keyboard interrupt, shutting down.\n')
 
     servo_scan.destroy_node()
     rclpy.shutdown()
