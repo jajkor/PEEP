06d911fa67bb26a53489df55935177b74da2ceb4
jajkor <play4rj@aol.com>
2024-08-08 18:12:51 -0400

    Added Pan/Tilt Servo for web cam

--- launch/rpi_launch.py
+++ launch/rpi_launch.py
@@ -41,10 +41,10 @@ def generate_launch_description():
             parameters=[{
                 'pan_pwm_channel': 1,
                 'pan_start_angle': 90,
-                'pan_axes': 6,
+                'pan_axes': 3,
                 'tilt_pwm_channel': 2,
                 'tilt_start_angle': 90,
-                'tilt_axes': 7,
+                'tilt_axes': 4,
             }]
         ),
     ])
