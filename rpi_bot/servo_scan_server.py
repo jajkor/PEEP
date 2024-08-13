0108fcaec604da6bee758389d1ea5f7b2e9e0ed6
jajkor <play4rj@aol.com>
2024-08-13 18:00:10 -0400

    Added Log statements for /motor_vel publication

--- rpi_bot/servo_scan_server.py
+++ rpi_bot/servo_scan_server.py
@@ -57,19 +57,17 @@ class Servo_Scan(Node):
         self.get_logger().info(f'List_Distance: {response.list_distance}')
         
         self.is_busy = False
-        self.range_listener.destroy()
         return response
 
 def main(args=None):
     rclpy.init(args=args)
 
     servo_scan = Servo_Scan()
-    #executor = MultiThreadedExecutor()
-    #executor.add_node(servo_scan)
+    executor = MultiThreadedExecutor()
+    executor.add_node(servo_scan)
 
     try:
-        servo_scan.spin()
-        #executor.spin()
+        executor.spin()
     except (KeyboardInterrupt, ExternalShutdownException):
         pass
     finally:
