38853424fe82a0ca039da73f2e84f1f7dce3d268
jajkor <play4rj@aol.com>
2024-08-14 22:13:44 -0400

    Servo scan test

--- rpi_bot/servo_scan_server.py
+++ rpi_bot/servo_scan_server.py
@@ -32,36 +32,25 @@ class Servo_Scan(Node):
         self.sg90.set_angle(self.start_angle)
 
         self.is_busy = False
-        self.distance = None
 
         #self.callback_group = ReentrantCallbackGroup()
-        self.srv = self.create_service(Scan, 'servo_scan', self.scan_callback)
+        self.scan_srv = self.create_service(Scan, 'servo_scan', self.scan_callback)
         #self.srv = self.create_service(Scan, 'servo_scan', self.scan_callback, callback_group=self.callback_group)
         #self.range_listener = self.create_subscription(Range, 'range', self.range_listener_callback, 10,  callback_group=self.callback_group)
 
-    def range_listener_callback(self, range_msg):
-        self.distance = range_msg.range
-
     def scan_callback(self, request, response):
-        if self.is_busy:
-            self.get_logger().info('Service is busy. Cannot handle request:')
-            return response
-        
-        self.is_busy = True
         self.get_logger().info(f"Servo Scan: {request.start_angle} to {request.stop_angle}")
 
         for i in range(int(request.start_angle), int(request.stop_angle), 10):
             self.sg90.set_angle(i)
             time.sleep(0.5)
             response.list_angle.append(round(float(self.sg90.get_angle()), 2))
-            response.list_distance.append(round(float(self.distance), 2))
 
         self.sg90.set_angle(self.start_angle)
 
         self.get_logger().info(f'List_Angle: {response.list_angle}')
         self.get_logger().info(f'List_Distance: {response.list_distance}')
         
-        self.is_busy = False
         return response
 
 def main(args=None):
@@ -77,7 +66,7 @@ def main(args=None):
     except (KeyboardInterrupt, ExternalShutdownException):
         pass
     finally:
-        servo_scan.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
